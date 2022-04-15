0f8528c7113e153ec8cde86edb6a9a865b9b69aa
sungsuhan <swhanssu@gmail.com>
2022-04-15 12:46:47 +0900

    2022-04-15 12:46 join

--- redux/reducers/userReducer.ts
+++ redux/reducers/userReducer.ts
@@ -1,48 +1,47 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit"
-// parameter
+import { createSlice } from "@reduxjs/toolkit"
 
-// payload
-export interface JoinPayload{
-    data:{
-        user: {
-            userid: string;
-            password: string;  
-            email: string;
-            name: string;  
-            phone: string;
-            birth: string;
-            address: string;
-        }
-    }
+export interface UserType{
+    userid: string;
+    password: string;  
+    email: string;
+    name: string;  
+    phone: string;
+    birth: string;
+    address: string;
 }
 
-// state
 export interface UserState{
-    userLoading: boolean;
-    userData: any;
+    loading: boolean;
+    data: UserType[];
     error: any;
-    token: null;
 }
 
 
 const initialState: UserState = {
-    userLoading: false,
-    userData: null,
-    error: null,
-    token: null
+    loading: false,
+    data: [],
+    error: null
 }
 
 const userSlice = createSlice({
     name: 'users',
     initialState,
     reducers: {
-        joinRequest(state: UserState, action: PayloadAction<JoinPayload>){ state.userLoading = true; state.error = null},
-        joinSuccess(state: UserState, action: PayloadAction<JoinPayload>){ state.userLoading = false; state.error = action.payload},
-        joinFailure(state: UserState, action: PayloadAction<JoinPayload>){ state.userLoading = false; state.error = action.payload}
+        joinRequest(state: UserState, payload){
+            alert('진행 2: 리듀서 내부 ') 
+            state.loading = true; 
+        },
+        joinSuccess(state: UserState, {payload}){ 
+            state.data = [...state.data, payload]
+            state.loading = false;
+            
+        },
+        joinFailure(state: UserState, {payload}){ 
+            state.data = payload;
+            state.loading = false;
+        }
     }
 })
 const { reducer, actions } = userSlice
-export const {
-    joinRequest, joinSuccess, joinFailure
-} = actions
+export const userActions = actions
 export default reducer
